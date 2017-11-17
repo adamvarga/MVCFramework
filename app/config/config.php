a7d9d29134e2065f533c6cd8fff387ed4332b234
Ádám Varga <adamvarga@Adams-MBP.fritz.box>
2017-11-17 12:14:55 +0100

    Add Core MVC Files with Orderstructure

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// DB Params
+define('DB_HOST','localhost');
+define('DB_USER','__YOUR_USER_');
+define('DB_PASS','__YOUR_PASS_');
+define('DB_NAME','__YOUR_DBNAME_');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', '__YOUR_URL_');
+// Site Name
+define('SITENAME','_YOUR_SITENAME_');
