cf88c94cc30bc8a8902307c2fdb33c95dd4f9e20
Ádám Varga <adamvarga@Adams-MBP.fritz.box>
2017-11-17 12:55:51 +0100

    Change readme and add new paragraph to index

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MVCFramework
+This is the core, license-free PHP MVC Framework with PDO connections.
+Please check the config and the .htaccess file, before you start the project.
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <h1><?php echo $data['title']; ?></h1>
+  <p>This is the core, license-free PHP MVC Framework with PDO connections.</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
